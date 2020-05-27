ef655348aa3d78caaecad78fdabfbb3bc197c9e9
Grawlin <65964657+Grawlin@users.noreply.github.com>
2020-05-27 11:38:10 -0300

    Add Bootrain Assignment 3

---
+++ BootrainAssignment3.py
@@ -0,0 +1,12 @@
+x = 1
+y = '1'
+
+print('Is x equal to y?:', x == y, '\n')
+print('Is it type of x equal to type of y?:', type(x) == type(y), '\n')
+
+int(y) 
+
+x = y = 1
+
+print('Is x equal to y?:', x == y, '\n')
+print('Is it type of x equal to type of y?:', type(x) == type(y), '\n')
