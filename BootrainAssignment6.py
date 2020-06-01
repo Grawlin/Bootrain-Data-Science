6864963a2c927eaca96c2e25f8059cc5ee88887b
Grawlin <65964657+Grawlin@users.noreply.github.com>
2020-06-01 12:11:28 -0300

    Add Bootrain Assignment 6

---
+++ BootrainAssignment6.py
@@ -0,0 +1,13 @@
+week = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+print(week, '\n')
+
+fruits = {'apple', 'mango', 'orange'}
+
+print(fruits, '\n')
+
+new_fruits = {'cherry', 'peach', 'apple', 'mango'}
+
+print(new_fruits.difference(fruits), '\n')
+
+print(new_fruits.intersection(fruits))
